006298fc0c501be905365aba56bb9e56084659d9
shahfaheem0001 <shahfaheem0099@gmail.com>
2025-04-27 01:01:15 +0530

    your commit message here

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,6 +1,7 @@
 const Listing=require("../models/listing")
 const mongoose = require("mongoose");
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mbxGeocoding = require('amapbox/mapbox-sdk/services/geocoding');
+ //const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken=process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
